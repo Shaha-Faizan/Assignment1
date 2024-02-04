4af8b1befa247fe5ea0e5392bcf8f5392b608afd
Shaha-Faizan <146312784+Shaha-Faizan@users.noreply.github.com>
2024-02-04 11:20:05 +0000

    .

--- CoreJavascript02/Q2.js
+++ CoreJavascript02/Q2.js
@@ -1,14 +1,39 @@
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
-
-const ages = [ 19,22,19,24,20,25,26,24,25,24]
-
-/** sort the array and find the min and max age */
-
-ages.sort((a,b) => a-b);   // for sorting array
+// Sort the array and find the min and max age
+ages.sort((a, b) => a - b);
 console.log(ages);
 
-const min_age = ages[0];    // for minimum age
-const max_age = ages[ages.length-1]  // for maximum age 
+const min_age = ages[0];
+const max_age = ages[ages.length - 1];
 
-console.log("Minimum age is:"+ min_age)
-console.log("maximum age is:"+ max_age)
+console.log("Minimum age is: " + min_age);
+console.log("Maximum age is: " + max_age);
+
+// Find median Age
+let medianAge;
+if (ages.length % 2 === 0) {
+    const median1 = ages[Math.floor(ages.length / 2) - 1];
+    const median2 = ages[Math.floor(ages.length / 2)];
+    medianAge = (median1 + median2) / 2;
+} else {
+    medianAge = ages[Math.floor(ages.length / 2)];
+}
+
+console.log("Median age is: " + medianAge);
+
+// Find the Average age
+const sumOfages = ages.reduce((sum, age) => sum + age, 0);
+const average = sumOfages / ages.length;
+console.log("Average age is: " + average);
+
+// Find Range
+const range = max_age - min_age;
+console.log("Range is: " + range);
+
+// Compare the value of (min - average) and (max - average) using abs() method
+const minimumDifference = Math.abs(min_age - average);
+const maximumDifference = Math.abs(max_age - average);
+
+console.log("Minimum Average Difference: ", minimumDifference);
+console.log("Maximum Average Difference: ", maximumDifference);
